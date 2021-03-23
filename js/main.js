cbb8e6adb9f7a53e9df878c9b268bf296b315f26
sverreskeggi <sverreskeggi@yandex.com>
2021-03-24 01:46:08 +0300

    feat(html, css): add modal menu mobile

--- js/main.js
+++ js/main.js
@@ -1,59 +1,34 @@
 'use strict';
 
+document.querySelector('.menu-button')
+  .addEventListener('click', () => {
+    document.querySelector('.navbar-bottom')
+      .classList.toggle('navbar-bottom--visible');
+})
+
 const mainSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
   direction: 'horizontal',
   loop: true,
-
-  // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
-
-  // Navigation arrows
   navigation: {
     nextEl: '.hotel-slider__button--next',
     prevEl: '.hotel-slider__button--prev',
   },
-
   effect: 'coverflow',
-
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
-
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
 });
 
 const newsletterSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
   direction: 'horizontal',
   loop: true,
-
-  // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
-
-  // Navigation arrows
   navigation: {
     nextEl: '.reviews-slider__button--next',
     prevEl: '.reviews-slider__button--prev',
   },
-
-  // effect: 'coverflow',
-
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
-
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
 });
